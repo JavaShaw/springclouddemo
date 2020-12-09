fe15e093e0c0f84116ea84c8027a8740e911c301
joeshaw <2551306415@qq.com>
2020-12-09 11:10:18 +0800

    ribbon集成hystrix出现问题待解决

---
+++ service-fegin/src/main/java/com/joeshaw/servicefegin/service/SchedualServiceHi.java
@@ -0,0 +1,12 @@
+package com.joeshaw.servicefegin.service;
+
+import com.joeshaw.servicefegin.controller.hystrix.SchedualServiceHiHystric;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(value = "SERVICE-HI",fallback = SchedualServiceHiHystric.class)
+public interface SchedualServiceHi {
+    @GetMapping(value = "/hi")
+    String sayHiFromClientOne(@RequestParam(value = "name") String name);
+}
