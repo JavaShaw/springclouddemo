f8f4ad1369513e9c3f845db2506e9ead6c71227f
joeshaw <2551306415@qq.com>
2020-12-09 11:11:02 +0800

    ribbon集成hystrix出现问题待解决

---
+++ service-fegin/src/main/java/com/joeshaw/servicefegin/controller/hystrix/SchedualServiceHiHystric.java
@@ -0,0 +1,13 @@
+package com.joeshaw.servicefegin.controller.hystrix;
+
+import com.joeshaw.servicefegin.service.SchedualServiceHi;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SchedualServiceHiHystric implements SchedualServiceHi {
+    @Override
+    public String sayHiFromClientOne(String name) {
+        return "sorry "+name;
+    }
+}
+
--- service-ribbon/src/main/java/com/joeshaw/serviceribbon/service/HelloServiceImpl.java
+++ service-ribbon/src/main/java/com/joeshaw/serviceribbon/service/HelloServiceImpl.java
@@ -1,5 +1,6 @@
 package com.joeshaw.serviceribbon.service;
 
+import com.netflix.ribbon.proxy.annotation.Hystrix;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -9,6 +10,7 @@ public class HelloServiceImpl implements HelloService {
     @Autowired
     RestTemplate restTemplate;
 
+    //@HystrixCommand(fallbackMethod = "hiError")
     public String hiService(String name) {
         return restTemplate.getForObject("http://SERVICE-HI/hi?name="+name,String.class);
     }
