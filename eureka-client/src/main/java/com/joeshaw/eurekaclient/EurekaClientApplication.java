370e37de012912588a7a8e4742c68547e1468cc4
joeshaw <2551306415@qq.com>
2020-12-15 13:47:58 +0800

    eureka-client集成sleuth实现链路追踪

--- eureka-client/src/main/java/com/joeshaw/eurekaclient/EurekaClientApplication.java
+++ eureka-client/src/main/java/com/joeshaw/eurekaclient/EurekaClientApplication.java
@@ -1,8 +1,10 @@
 package com.joeshaw.eurekaclient;
 
+import brave.sampler.Sampler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -12,4 +14,9 @@ public class EurekaClientApplication {
 		SpringApplication.run(EurekaClientApplication.class, args);
 	}
 
+
+	@Bean
+	public Sampler defaultSampler() {
+		return Sampler.ALWAYS_SAMPLE;
+	}
 }
--- eureka-client/src/main/java/com/joeshaw/eurekaclient/controller/HiController.java
+++ eureka-client/src/main/java/com/joeshaw/eurekaclient/controller/HiController.java
@@ -1,5 +1,8 @@
 package com.joeshaw.eurekaclient.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.event.Level;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -7,12 +10,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HiController {
+    private static final Logger LOG = LoggerFactory.getLogger(HiController.class);
 
     @Value("${server.port}")
     String port;
 
     @RequestMapping("/hi")
     public String home(@RequestParam(value = "name", defaultValue = "张三") String name) {
+        LOG.info("calling trace service-hi ",Level.INFO);
         return "hi " + name + " ,i am from port:" + port;
     }
 }
